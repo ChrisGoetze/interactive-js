a363dad566676cebc2a50ea3c2e7dc94283e408e
ChrisGoetze <ctgoetze@gmail.com>
2018-02-05 11:30:51 -0500

    add app.js and index.html

---
+++ app.js
@@ -0,0 +1,26 @@
+var newColor = document.getElementsByTagName("li");
+
+changeColor();
+
+function changeColor() {
+	var color = prompt("Choose a color for the list.  Blue, Green, or Red");
+	color = color.toLowerCase();
+	
+	if(color === "blue") {
+		for (var i = 0; i < newColor.length; i++) {
+			newColor[i].style.color = "blue";
+		}
+		
+	} else if (color === "green") {
+		for (var i = 0; i < newColor.length; i++) {
+			newColor[i].style.color = "green";
+		}
+	} else if (color === "red") {
+		for (var i = 0; i < newColor.length; i++) {
+			newColor[i].style.color = "red";
+		}
+	} else {
+		alert("Please choose a valid color");
+		changeColor();
+	}
+}
